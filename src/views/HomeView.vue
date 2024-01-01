ad0daa5d628eb1124a533fbbe81e0a10bc70dcdc
Hanshi <hanshitrash00@proton.me>
2024-01-02 02:25:23 +0800

    add(views): add home page template.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+<div class="container mx-auto border">
   <main>
-    <TheWelcome />
+    <div class="grid grid-rows-2 grid-flow-col border">
+      <div>
+        <h1 class="text-2xl text-center">
+        Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dignissimos, aliquam ratione exercitationem, illo temporibus dicta in dolorem sunt corporis eius assumenda dolor officia porro molestiae quibusdam voluptatibus quod. Aperiam, consectetur?
+        </h1>
+      </div>
+      <div class="grid grid-rows-2 grid-flow-col gap-2 border">
+        <div>Center</div>
+        <div class="grid grid-cols-2 gap-2">
+          <div>Center Div</div>
+          <div>Center Div</div>
+        </div>
+      </div>
+    </div>
   </main>
+</div>
 </template>
